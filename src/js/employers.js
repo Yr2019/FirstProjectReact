f1b552d4524f1d3d734cb3166045f430a081826b
Yurii <oliinyk0590@gmail.com>
2019-06-10 12:31:29 +0300

    Second-Babel-Fix-1

--- src/js/employers.js
+++ src/js/employers.js
@@ -1,14 +1,8 @@
-"use strict";
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+let employersNames = employers.filter((employers) => {
+  return employers.length > 0
 });
-exports.employersNames = void 0;
-var employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
-var employersNames = employers.filter(function (employers) {
-  return employers.length > 0;
-});
-exports.employersNames = employersNames;
-exports.employersNames = employersNames = employersNames.map(function (item) {
-  return item.toLowerCase().trim();
-});
+employersNames = employersNames.map((item) => item.toLowerCase().trim());
+
+export {employersNames};
